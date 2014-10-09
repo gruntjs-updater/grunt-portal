82513a88dee49aaca9c7cae05e910f7b3632fb3a
Marcel Miranda <m@reaktivo.com>
2014-10-09 12:52:45 -0500

    Update to be silent

---
+++ routes/component.js
@@ -0,0 +1,25 @@
+
+var express = require('express');
+var hbs = require('express-handlebars');
+var path = require('path');
+
+module.exports = function(options) {
+	var app = express()
+	var assetsDir = path.join(__dirname, '..', 'lib', 'server-component');
+
+	app.engine('hbs', hbs({ extname: '.hbs' }));
+	app.set('view engine', 'hbs');
+	app.set('views', assetsDir);
+
+	app.use(express.static(assetsDir));
+
+	app.get('*', function(req, res, next) {
+		console.log('HIT', req.params);
+	    res.render('componentTestTemplate', {
+	        component: req.params[0]
+	    });
+	});
+
+	return app;
+
+}
